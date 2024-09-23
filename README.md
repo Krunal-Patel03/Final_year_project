f8657f3a7ddfc58e87b57570cbd2260ceb76cbf4
Krunal-Patel03 <isamaliyakrunal@gmail.com>
2024-09-23 22:54:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Final_year_project
+# Final_year_project
---
+++ stock-finhub/src/App.jsx
@@ -0,0 +1,35 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import { StockOverviewPage } from './pages/StockOverviewPage';
+import { StockDetailPage } from './pages/StockDetailPage';
+import { WatchListContextProvider } from './context/WatchListContext';
+import { ProductsList } from './pages/Products/ProductsList';
+import { ProductDetail } from './pages/ProductDetail';
+import { ScrollToTop } from './components/ScrollToTop';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
+import { ToastContainer } from 'react-toastify';
+
+function App() {
+  return (
+    <div className='container'>
+      <WatchListContextProvider>
+        <BrowserRouter>
+          <ScrollToTop /> {/* Moved inside BrowserRouter */}
+          <ToastContainer />
+          <Routes>
+            <Route path="/" element={<StockOverviewPage />} />
+            <Route path="/detail/:symbol" element={<StockDetailPage />} />
+            <Route path="/products" element={<ProductsList />} />
+            <Route path="/products/:id" element={<ProductDetail />} />
+
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Routes>
+        </BrowserRouter>
+      </WatchListContextProvider>
+    </div>
+  );
+}
+
+export default App;
